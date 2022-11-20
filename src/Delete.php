800c4474e1d9126e579cd6e858c1ed5ecb707107
Martin Bamber <mbamber1986@gmail.com>
2022-11-20 11:36:45 +0000

    Added src folder

---
+++ src/Delete.php
@@ -0,0 +1,52 @@
+<?php
+namespace SnorkelWeb\QueryBuilder;
+
+use SnorkelWeb\Database\Connection;
+use SnorkelWeb\QueryBuilder\Traits\From;
+use SnorkelWeb\QueryBuilder\Traits\Params;
+use SnorkelWeb\QueryBuilder\Traits\Where;
+
+class Delete extends Connection
+{
+  
+    private $sql;
+    private $stmt;
+
+    // traits
+
+    use From;
+    use Params;
+    use Where;
+
+    public function Delete()
+    {
+        $this->sql = "Delete ";
+        return $this;
+
+    }
+
+    // insert names
+
+    public function prepare($sql)
+    {
+       return $this->pdo->prepare($sql);
+    }
+
+
+    public function save()
+    {        
+        /**
+         * Retrieve WHere Values if neeeded.
+        */
+         $this->Fetchwhere();
+
+        $this->stmt = $this->prepare($this->sql);
+    // Param Binder
+       $this->parambinder();
+        // Execute the final Script;
+        echo $this->sql;
+         $this->stmt->execute();
+         return $this;
+    }
+
+}
